5cfa312c38384a9e430698710e00dbac2b151b03
Gildas <gildas.lormeau@gmail.com>
2025-03-21 20:36:53 +0100

    translate the icon

--- resources/script.js
+++ resources/script.js
@@ -8,13 +8,16 @@
 
 const CLASS_DARK = "dark-theme";
 const CLASS_LIGHT = "light-theme";
+const STYLE_TRANSFORM_ICON_DARK = "rotate(180deg) translateY(-1px)";
+const STYLE_TRANSFORM_ICON_LIGHT = "translateY(-1px)";
 const icon = document.querySelector("svg");
 const documentElement = document.documentElement;
 documentElement.className = matchMedia("(prefers-color-scheme: dark)").matches ? CLASS_DARK : CLASS_LIGHT;
 icon.style.display = "";
-icon.style.transform = documentElement.className === CLASS_DARK ? "scale(-1, -1)" : "";
+icon.style.transform = documentElement.className === CLASS_DARK ? STYLE_TRANSFORM_ICON_DARK : STYLE_TRANSFORM_ICON_LIGHT;
 icon.addEventListener("click", () => {
-    icon.style.transform = icon.style.transform ? "" : "scale(-1, -1)";
-    documentElement.className = documentElement.className === CLASS_DARK ? CLASS_LIGHT : CLASS_DARK;
+    const darkThemeSelected = documentElement.className === CLASS_DARK;
+    documentElement.className = darkThemeSelected ? CLASS_LIGHT : CLASS_DARK;
+    icon.style.transform = darkThemeSelected ? STYLE_TRANSFORM_ICON_LIGHT : STYLE_TRANSFORM_ICON_DARK;
 });
 document.currentScript.remove();
